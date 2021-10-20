3eacbfea3079f3dd206d48827d62eac1985272ee
Sietze <s.t.gelderloos@student.tue.nl>
2021-10-20 23:19:25 +0200

    does not run on my machine... (ubuntu on WSL 2)

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 # taken from:
 # https://github.com/JohnSnowLabs/nlu/blob/master/examples/colab/Training/multi_lingual/binary_text_classification/NLU_multi_lingual_training_sentiment_classifier_demo_twitter.ipynb
 
+import nlu
 import pandas as pd
 train_path = "./twitter_data_multi_lang.csv"
 
@@ -14,3 +15,22 @@ train_df, test_df = train_test_split(train_df, test_size=0.2)
 
 print("Loaded training data:")
 print(train_df)
+
+print("Downloading ")
+trainable_pipe = nlu.load('xx.embed_sentence.labse train.sentiment')
+# We need to train longer and user smaller LR for NON-USE based sentence embeddings usually
+# We could tune the hyperparameters further with hyperparameter tuning methods like gridsearch
+# Also longer training gives more accuracy
+trainable_pipe['sentiment_dl'].setMaxEpochs(60)  
+trainable_pipe['sentiment_dl'].setLr(0.005)
+print("Now training...")
+fitted_pipe = trainable_pipe.fit(train_df)
+# predict with the trainable pipeline on dataset and get predictions
+preds = fitted_pipe.predict(train_df,output_level='document')
+
+#sentence detector that is part of the pipe generates sone NaNs. lets drop them first
+preds.dropna(inplace=True)
+from sklearn.metrics import classification_report
+print(classification_report(preds['y'], preds['trained_sentiment']))
+
+print(preds)
--- train.sh
+++ train.sh
@@ -1,2 +1,4 @@
+#wget https://setup.johnsnowlabs.com/nlu/colab.sh -O - | bash
+rm twitter_data_multi_lang.csv
 wget http://ckl-it.de/wp-content/uploads/2021/02/twitter_data_multi_lang.csv
 python train.py
