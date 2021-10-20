9c547aaa1cc8dd128aa4a8245a37a4a989a3f87f
Sietze <s.t.gelderloos@student.tue.nl>
2021-10-20 15:39:39 +0200

    now using snscrape python wrapper, stripping newlines, searching for all keywords in a list, improved progress overview in CLI

--- parse-twitter-data.py
+++ parse-twitter-data.py
@@ -1,14 +1,35 @@
 import json
+import snscrape.modules.twitter as sntwitter
 
-in_file = 'twitter-data'
-output_file = 'tweets.txt'
+max_tweets = 100
 
-with open(in_file, 'r') as twitter_data, \
-     open(output_file, 'a') as f:
-    for line in twitter_data.readlines():
-        tweet = json.loads(line)['content']
-        # remove newlines in order to have
-        # one tweet per line in the output file
+output_file = "tweets_dutch.txt"
+
+def scrapeByKeyword(query):
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(keyword).get_items()):
+        if i % 10 == 0:
+            print("\r tweets scraped for keyword '" + keyword + "': " + str(i) + "/" + str(max_tweets) + "       ", end = "")
+        if i >= max_tweets:
+            break
+        tweets_list.append(tweet.content)
+
+
+# Creating list to append tweet data to
+tweets_list = []
+# temporary. TODO load from file.
+keywords = ["hond", "kat", "meneer"]
+
+for keyword in keywords:
+    scrapeByKeyword(keyword)
+    print()
+
+print()
+print("Beginning writing Dutch tweets to file...") 
+
+with open(output_file, 'w') as f:
+    for tweet in tweets_list:
         f.write(tweet.replace('\n', '') + '\n')
 
-
+print("Done.")
+print()
--- scrape-and-translate.sh
+++ scrape-and-translate.sh
@@ -1,7 +1,7 @@
 rm tweets_dutch.txt
 rm tweets_english.txt
 
-snscrape -n 10 -f '{content}' twitter-search iets > tweets_dutch.txt
-
+#snscrape -n 10 -f '{content}' twitter-search iets > tweets_dutch.txt
+python parse-twitter-data.py
 python translate.py
 
