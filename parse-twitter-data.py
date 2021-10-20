f92a65d09088ca9a7f60528c9e6b69223b514795
Sietze <s.t.gelderloos@student.tue.nl>
2021-10-20 15:46:33 +0200

    cleanup

--- parse-twitter-data.py
+++ parse-twitter-data.py
@@ -5,9 +5,11 @@ max_tweets = 100
 
 output_file = "tweets_dutch.txt"
 
-def scrapeByKeyword(query):
+# function to scrape tweets by keyword
+def scrapeByKeyword(keyword):
     # Using TwitterSearchScraper to scrape data and append tweets to list
     for i, tweet in enumerate(sntwitter.TwitterSearchScraper(keyword).get_items()):
+        # print progress to CLI
         if i % 10 == 0:
             print("\r tweets scraped for keyword '" + keyword + "': " + str(i) + "/" + str(max_tweets) + "       ", end = "")
         if i >= max_tweets:
@@ -20,16 +22,17 @@ tweets_list = []
 # temporary. TODO load from file.
 keywords = ["hond", "kat", "meneer"]
 
+# Perform the scrape, keyword by keyword. TODO get from file.
 for keyword in keywords:
     scrapeByKeyword(keyword)
     print()
 
-print()
+# write contents of all scraped tweets to the output file.
 print("Beginning writing Dutch tweets to file...") 
-
 with open(output_file, 'w') as f:
     for tweet in tweets_list:
-        f.write(tweet.replace('\n', '') + '\n')
+        # replace newlines with spaces
+        f.write(tweet.replace('\n', ' ') + '\n')
 
 print("Done.")
 print()
